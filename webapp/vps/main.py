eaf8d13a627340fb31d57820d4e91a60c7f04870
erik-metz <erik.metz.info@gmail.com>
2026-09-11 13:24:51 +0200

    Refactor database connection setup in FastAPI: Updated environment variable names for database configuration to improve clarity and maintainability. This change enhances the initialization process for the asynchronous connection pool during application startup.

--- webapp/vps/main.py
+++ webapp/vps/main.py
@@ -7,11 +7,11 @@ from api.v1.router import api_router as v1_router
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    user = os.getenv("POSTGRES_USER", "postgres")
-    password = os.getenv("POSTGRES_PASSWORD", "")
-    host = os.getenv("POSTGRES_HOST", "timescaledb")
-    port = os.getenv("POSTGRES_PORT", "5432")
-    db = os.getenv("POSTGRES_DB")
+    user = os.getenv("DB_USER")
+    password = os.getenv("DB_PASSWORD", "")
+    host = os.getenv("DB_HOST", "timescaledb")
+    port = os.getenv("DB_PORT", "5432")
+    db = os.getenv("DB_NAME")
 
     conninfo = f"postgresql://{user}:{password}@{host}:{port}/{db}"
 
