f120de9a74fe4332e41b129683f230315cb83207
erik-metz <erik.metz.info@gmail.com>
2026-09-13 14:56:06 +0200

    fix

--- www/open-ried-sens/env.ts
+++ www/open-ried-sens/env.ts
@@ -11,7 +11,7 @@ export const env = createEnv({
       .startsWith("open_ried_sens_admin_"),
   },
   client: {
-    NEXT_PUBLIC_PUBLISHABLE_KEY: z.string().min(1),
+    // NEXT_PUBLIC_PUBLISHABLE_KEY: z.string().min(1),
   },
   // If you're using Next.js < 13.4.4, you'll need to specify the runtimeEnv manually
   runtimeEnv: {
